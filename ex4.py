f1fa9ff6ebb22649b6b8accaf388305f0b576609
Pedro Henrique Lopes Da Silva <p.lopes.judoca@gmail.com>
2025-03-20 18:14:59 -0400

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,7 @@
+#pedir ao usuario um numero e usando o comando if determine se é par, impar ou 0
+nun=int(input("inserir numero"))
+if nun % 2 == 0:
+    print=input("Esse numero é par")     
+else:
+    print=input("Esse numero é impar")
+    
---
+++ index.py
@@ -0,0 +1,63 @@
+# #isso é um comentario
+# # print("mensagem")
+# idade=6
+# altura=0.50
+# peso=5.950
+# nome="Francisco"
+# print(idade,altura,peso,nome)lreg
+
+
+# nome=input("insira seu nome") #input texto
+# idade=int(input("insira idade")) #int numero inteiro
+# peso=float(input("insira seu peso")) #numero real
+# print(f" nome, {nome} idade, {idade} anos, {peso} kg") #mostar para o usuario
+
+# pedaco_de_pizza=5.0 #declaração de variavel
+# cliente="john" #declaração de variavel
+# print(type(pedaco_de_pizza)) #mostar tipo de variavel
+# print(type(cliente)) #mostar tipo de variavel
+
+
+# pedaco_de_pizza=input("informe quantos pedaços comeu") #type mostra o tipo da variavel 
+# print(type(pedaco_de_pizza)) #print mostra para o usuario
+# print=input(f"Voce comeu {pedaco_de_pizza}") #input coloca o texto logo apos a variavel #o (f" coloque para mostar o valor de uma variavel para o usuario
+
+# a=4
+# A="Salty"
+# print(a)
+# print(A)
+
+# fruta1, fruta2, fruta3= "Laranja", "Banana", "Maça"  
+# print(fruta1, fruta2, fruta3)
+
+# primeiro_numero=5
+# segundo_numero=10
+# print(primeiro_numero+segundo_numero)
+
+# nome=input("Qual o nome do vingador mais forte?")
+# print(f" Olá, {nome}!")
+# if nome == "hulk":
+#     print("Bem vindo vingador mais forte")
+# else:
+#     print("acesso negado!")
+
+# if condição
+#     codigo
+# else
+#     outro codigo
+    
+# x=5
+# if x>2 and x<10:
+#     print("O numero está entre 2 e 10") # AND Ambas as consiçoes devem ser verdadeiras 
+
+
+# x=5
+# if x<2 or x>4:
+#     print("O numero é menor qq 2 ou maior que 4") # OR apenas uma condição deve ser atendida ou verdadeira 
+
+x=5
+if not x>10: #Inverte a condição (x nao é maior que 10)
+    print("o numero nao é maior que 10")
+else
+
+
