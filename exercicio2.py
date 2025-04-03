03984d5e714f4ebb75fca3328a2d258f82f77b83
Pedro Henrique Lopes Da Silva <p.lopes.judoca@gmail.com>
2025-04-03 14:42:53 -0400

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,3 @@
+nome = input("digite seu nome:")
+idade = input("digite sua idade:")
+print(f"ola {nome}! você tem {idade} anos")
--- index.py
+++ index.py
@@ -1,63 +1,61 @@
-# #isso é um comentario
-# # print("mensagem")
-# idade=6
-# altura=0.50
-# peso=5.950
-# nome="Francisco"
-# print(idade,altura,peso,nome)lreg
+# # #isso é um comentario
+# # print("noooooooooooooo, é muito cremosu")
 
+# # idade = 6 
+# # altura = 0.50
+# # peso = 5.900
+# # nome = "Francisco"
+# # print(idade, altura, peso, nome)
 
-# nome=input("insira seu nome") #input texto
-# idade=int(input("insira idade")) #int numero inteiro
-# peso=float(input("insira seu peso")) #numero real
-# print(f" nome, {nome} idade, {idade} anos, {peso} kg") #mostar para o usuario
+# # nome = input("insira seu nome:")
+# # idade = int(input("insira sua idade:"))
+# # peso = float(input("insira seu peso:"))
+# # print(f"{nome}, {idade} anos, {peso} kg")
 
-# pedaco_de_pizza=5.0 #declaração de variavel
-# cliente="john" #declaração de variavel
-# print(type(pedaco_de_pizza)) #mostar tipo de variavel
-# print(type(cliente)) #mostar tipo de variavel
+# pedaco_pizza = 5
+# cliente = "jhon"
+# print(type(pedaco_pizza))
+# print(type(cliente))
 
+# pedaco_pizza = input("informe quantos pedacos comeu")
+# print(type(pedaco_pizza))
 
-# pedaco_de_pizza=input("informe quantos pedaços comeu") #type mostra o tipo da variavel 
-# print(type(pedaco_de_pizza)) #print mostra para o usuario
-# print=input(f"Voce comeu {pedaco_de_pizza}") #input coloca o texto logo apos a variavel #o (f" coloque para mostar o valor de uma variavel para o usuario
-
-# a=4
-# A="Salty"
+# a = 4
+# A = "Sally"
 # print(a)
 # print(A)
 
-# fruta1, fruta2, fruta3= "Laranja", "Banana", "Maça"  
-# print(fruta1, fruta2, fruta3)
+# fruta1,fruta2,fruta3 = "laranja", "banana", "maça"
+# print(fruta1,fruta2,fruta3)
 
-# primeiro_numero=5
-# segundo_numero=10
-# print(primeiro_numero+segundo_numero)
+# primeiro_numero = 5
+# segundo_numero = 10
+# print(primeiro_numero + segundo_numero)
 
-# nome=input("Qual o nome do vingador mais forte?")
-# print(f" Olá, {nome}!")
-# if nome == "hulk":
-#     print("Bem vindo vingador mais forte")
+# nome = input("Qual o vingador mais forte ?")
+# print(f"Olá, {nome}!")
+# if nome == "hulk" :
+#     print("bem vindo vingador mais forte!")
 # else:
-#     print("acesso negado!")
+#     print("acesso negado")
 
-# if condição
-#     codigo
-# else
-#     outro codigo
+# x = 5
+# if x > 2 and x < 5:
+#     print("o numero esta entre 2 e 10.")
+# else:
+#     print("o numero não esta entre 2 e 10.")
     
+# x = 5
+# if x > 2 or x < 4:
+#     print("o numero é menor que 2 ou maior que 4.")
+
+# x = 5
+# if not x > 10:
+#     print("o numero nao e maior que 10.")
+
 # x=5
-# if x>2 and x<10:
-#     print("O numero está entre 2 e 10") # AND Ambas as consiçoes devem ser verdadeiras 
-
-
-# x=5
-# if x<2 or x>4:
-#     print("O numero é menor qq 2 ou maior que 4") # OR apenas uma condição deve ser atendida ou verdadeira 
-
-x=5
-if not x>10: #Inverte a condição (x nao é maior que 10)
-    print("o numero nao é maior que 10")
-else
-
-
+# y=8 
+# if x>2 and (y>10 or not x==5):
+#     print("Condicao complexa atendida.")
+# else:
+#     print("Condicao nao atendida.")
